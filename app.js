51319a127d0a098b92447fc16b16fe81a85ddb31
Paola Guarasci <paolaguarasci@gmail.com>
2018-08-19 17:28:25 +0200

    html5boilerplate manul add

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
+import "./src/js/main.js";
+import "./src/scss/main.scss";
+
 function requireAll(r) {
   r.keys().forEach(r);
 }
 
-// Import all JS files
-requireAll(require.context("./src/js", true, /\.js$/));
-// Import all CSS/SASS/SCSS files
-requireAll(require.context("./src/scss", true, /\.scss$/));
 // Import all IMG files
 const img = requireAll(
   require.context("./src/img", true, /\.(png|svg|jpg|gif)$/)
 );
+
 // Import all HTML files
 requireAll(require.context("./src/", true, /\.(html|htm|)$/));
